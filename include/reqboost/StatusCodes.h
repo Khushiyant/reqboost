5456c290d89dda079c9318e551dd507da06dc42a
Khushiyant <khushiyant2002@gmail.com>
2024-08-22 22:56:27 +0530

    chore: add mapped status codes

---
+++ include/reqboost/StatusCodes.h
@@ -0,0 +1,16 @@
+#ifndef REQBOOST_HPP
+#define REQBOOST_HPP
+
+#include <string>
+#include <map>
+#include <vector>
+
+namespace Reqboost {
+    // Forward declaration of the httpStatusCodes map
+    extern std::map<int, std::vector<std::string>> httpStatusCodes;
+
+    // Function to get the HTTP status code from its name
+    int getStatusCode(const std::string &name);
+}  // namespace Reqboost
+
+#endif  // REQBOOST_HPP
