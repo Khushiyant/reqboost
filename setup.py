1682f84414bfd8a0407390128c135a37c89b69b9
Khushiyant <khushiyant2002@gmail.com>
2024-08-24 01:34:05 +0530

    chore: add poco--search setup.py

--- setup.py
+++ setup.py
@@ -12,40 +12,51 @@ def get_pkg_config_paths(package):
     try:
         include_dirs = subprocess.check_output(
             ["pkg-config", "--cflags", package]
-        ).decode().strip().split()[1:]  # Skip '-I' flag
+        ).decode().strip().split()
+        include_dirs = [d[2:] for d in include_dirs if d.startswith('-I')]
+
         library_dirs = subprocess.check_output(
             ["pkg-config", "--libs", package]
-        ).decode().strip().split()[1:]  # Skip '-L' flag
-        include_dirs = [d[2:] for d in include_dirs if d.startswith('-I')]
+        ).decode().strip().split()
         library_dirs = [d[2:] for d in library_dirs if d.startswith('-L')]
+
         return include_dirs, library_dirs
     except subprocess.CalledProcessError:
         return [], []
 
-# Get paths for libcurl
+# Get paths for libcurl and Poco
 if sys.platform == "win32":
     # On Windows, use environment variables or provide default paths
-    curl_include_dir = os.getenv('CURL_INCLUDE_DIR', 'C:\curl\include')
-    curl_lib_dir = os.getenv('CURL_LIB_DIR', 'C:\curl\lib')
+    curl_include_dir = os.getenv('CURL_INCLUDE_DIR', 'C:\\curl\\include')
+    curl_lib_dir = os.getenv('CURL_LIB_DIR', 'C:\\curl\\lib')
+    poco_include_dir = os.getenv('POCO_INCLUDE_DIR', 'C:\\poco\\include')
+    poco_lib_dir = os.getenv('POCO_LIB_DIR', 'C:\\poco\\lib')
     curl_include_dirs = [curl_include_dir]
     curl_library_dirs = [curl_lib_dir]
+    poco_include_dirs = [poco_include_dir]
+    poco_library_dirs = [poco_lib_dir]
 else:
     # On Unix-like systems, use pkg-config to find paths
     curl_include_dirs, curl_library_dirs = get_pkg_config_paths('libcurl')
+    poco_include_dirs, poco_library_dirs = get_pkg_config_paths('poco')
+
     # Add default paths in case pkg-config is not available
     if not curl_include_dirs or not curl_library_dirs:
         curl_include_dirs = ["/opt/homebrew/opt/curl/include", "include/reqboost/"]
         curl_library_dirs = ["/opt/homebrew/opt/curl/lib"]
+    if not poco_include_dirs or not poco_library_dirs:
+        poco_include_dirs = ["/opt/homebrew/opt/poco/include"]
+        poco_library_dirs = ["/opt/homebrew/opt/poco/lib"]
 
 ext_modules = [
     Pybind11Extension(
         "reqboost",
         sorted(glob.glob("src/*.cpp")),
-        include_dirs=curl_include_dirs,
-        libraries=["curl"],
-        library_dirs=curl_library_dirs,
+        include_dirs=curl_include_dirs + poco_include_dirs,
+        libraries=["curl"] + ["PocoFoundation", "PocoNet"],  # Adjust library names as needed
+        library_dirs=curl_library_dirs + poco_library_dirs,
         define_macros=[("VERSION_INFO", __version__)],
-        extra_compile_args=["-std=c++11"],  # Adjust as necessary
+        extra_compile_args=["-std=c++14"],  # Adjust as necessary
     ),
 ]
 
@@ -55,7 +66,7 @@ setup(
     author="Khushiyant",
     author_email="khushiyant2002@gmail.com",
     url="https://github.com/Khushiyant/reqboost",
-    description="Python package for HTTP requests based on C++ libcurl",
+    description="Python package for HTTP requests based on C++ libcurl and Poco",
     long_description="",
     license="Apache-2.0",
     classifiers=[
