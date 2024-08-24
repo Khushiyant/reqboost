44415678432a1a978f1298076070541da7209b88
Vidhu Mathur <vidhumathur2002@gmail.com>
2024-08-25 04:08:41 +0530

    add: parsedurl model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -40,3 +40,26 @@ def test_response_repr():
     response = Response()
     response.status_code = 404
     assert repr(response) == "<Response [404]>"
+
+def test_parsedurl_initialization():
+    parsed_url = ParsedURL()
+    assert parsed_url.scheme == ""
+    assert parsed_url.host == ""
+    assert parsed_url.path == ""
+    assert parsed_url.query == ""
+    assert parsed_url.fragment == ""
+
+def test_parsedurl_attributes():
+    parsed_url = ParsedURL()
+
+    parsed_url.scheme = "https"
+    parsed_url.host = "example.com"
+    parsed_url.path = "/index"
+    parsed_url.query = "a=1"
+    parsed_url.fragment = "section1"
+
+    assert parsed_url.scheme == "https"
+    assert parsed_url.host == "example.com"
+    assert parsed_url.path == "/index"
+    assert parsed_url.query == "a=1"
+    assert parsed_url.fragment == "section1"
