f3aab394919ea1bf30a647bc591b5bff38786c50
Khushiyant <khushiyant2002@gmail.com>
2024-08-25 23:02:41 +0530

    chore: add logger--class

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -45,5 +45,48 @@ namespace Reqboost {
 
             return parsed;
         }
+
+        // LogMessage class definitions
+        LogMessage::LogMessage(LogLevel level, const std::string &message)
+            : level(level), message(message) {}
+
+        LogLevel LogMessage::getLevel() const
+        {
+            return level;
+        }
+
+        std::string LogMessage::getMessage() const
+        {
+            return message;
+        }
+
+        // ConsoleSink class definition
+        void ConsoleSink::log(const LogMessage &message)
+        {
+            std::cout << message.getMessage() << std::endl;
+        }
+
+        // FileSink class definitions
+        FileSink::FileSink(const std::string &filename) : file(filename, std::ios::app) {}
+
+        void FileSink::log(const LogMessage &message)
+        {
+            file << message.getMessage() << std::endl;
+        }
+
+        // Logger class definitions
+        void Logger::addSink(std::shared_ptr<LogSink> sink)
+        {
+            sinks.push_back(sink);
+        }
+
+        void Logger::log(LogLevel level, const std::string &message)
+        {
+            LogMessage logMessage(level, message);
+            for (const auto &sink : sinks)
+            {
+                sink->log(logMessage);
+            }
+        }
     }
 }
