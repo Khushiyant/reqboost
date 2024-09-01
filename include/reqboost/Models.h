fda26a102bcb6d3cee288310cf97ad0abb056655
Vidhu Mathur <vidhumathur2002@gmail.com>
2024-09-02 01:57:30 +0530

    add: requestencoding, requesthooks and multipartencoder classes

--- include/reqboost/Models.h
+++ include/reqboost/Models.h
@@ -5,7 +5,13 @@
 
 #include "Exceptions.h"
 #include "StatusCodes.h"
-
+#include <iostream>
+#include <map>
+#include <string>
+#include <any>
+#include <vector>
+#include <utility>
+#include <functional>
 
 namespace Reqboost
 {
@@ -117,6 +123,38 @@ namespace Reqboost
 
                 // Constructor
                 Response();
+        };
+
+        class RequestEncodingMixin
+        {
+            public:
+                std::string path_url(const std::string &url);
+                std::string encoding_params(const std::map<std::string, std::string> &data);
+
+            private:
+                ParsedURL urlsplit(const std::string &url);    
+        };
+        
+        class RequestHooksMixin
+        {
+            public:
+                void register_hook(const std::string &event, const std::function<void()> &hook);
+                bool deregister_hook(const std::string &event, const std::function<void()> &hook);
+            
+            private:
+                std::map<std::string, std::vector<std::function<void()>>> hooks;
+        };
+
+        class MultipartEncoder
+        {
+        public:
+            MultipartEncoder();
+            std::pair<std::string, std::string> encode_multipart_formdata(const std::map<std::string, std::string> &fields, const std::map<std::string, std::pair<std::string, std::string>> &files);
+
+        private:
+            std::string generate_boundary();
+            std::string create_form_field(const std::string &name, const std::string &value);
+            std::string create_file_field(const std::string &name, const std::pair<std::string, std::string> &file_info);
         };
     } // namespace Models
 } // namespace Reqboost
