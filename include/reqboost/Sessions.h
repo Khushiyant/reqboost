ca318448e1c4d2a63b971b96964c2e301708b72e
Khushiyant <khushiyant2002@gmail.com>
2024-09-08 16:16:54 +0530

    chore: remove twine base--dependency

--- include/reqboost/Sessions.h
+++ include/reqboost/Sessions.h
@@ -25,95 +25,95 @@ namespace Reqboost
 
             /**
              * @brief Base request method
-             * 
-             * @param method 
-             * @param url 
-             * @param kwargs 
-             * @return Models::Response 
+             *
+             * @param method
+             * @param url
+             * @param kwargs
+             * @return Models::Response
              */
             Models::Response request(const std::string &method, const std::string &url, const py::kwargs kwargs);
 
             /**
              * @brief GET request
-             * 
-             * @param url 
-             * @param params 
-             * @param kwargs 
-             * @return Models::Response 
+             *
+             * @param url
+             * @param params
+             * @param kwargs
+             * @return Models::Response
              */
             Models::Response get(const std::string &url, const py::dict params = py::none(), py::kwargs kwargs = {});
 
             /**
              * @brief POST request
-             * 
-             * @param url 
-             * @param data 
-             * @param kwargs 
-             * @return Models::Response 
+             *
+             * @param url
+             * @param data
+             * @param kwargs
+             * @return Models::Response
              */
             Models::Response post(const std::string &url, const py::dict data = py::none(), py::kwargs kwargs = {});
 
             /**
              * @brief PUT request
-             * 
-             * @param url 
-             * @param data 
-             * @param kwargs 
-             * @return Models::Response 
+             *
+             * @param url
+             * @param data
+             * @param kwargs
+             * @return Models::Response
              */
             Models::Response put(const std::string &url, const py::dict data = py::none(), py::kwargs kwargs = {});
 
             /**
-             * @brief PATCH request 
-             * 
-             * @param url 
-             * @param data 
-             * @param kwargs 
-             * @return Models::Response 
+             * @brief PATCH request
+             *
+             * @param url
+             * @param data
+             * @param kwargs
+             * @return Models::Response
              */
             Models::Response patch(const std::string &url, const py::dict data = py::none(), py::kwargs kwargs = {});
 
             /**
-             * @brief DELETE request 
-             * 
-             * @param url 
-             * @param kwargs 
-             * @return Models::Response 
+             * @brief DELETE request
+             *
+             * @param url
+             * @param kwargs
+             * @return Models::Response
              */
             Models::Response delete_(const std::string &url, py::kwargs kwargs = {});
 
             /**
-             * @brief HEAD request 
-             * 
-             * @param url 
-             * @param kwargs 
-             * @return Models::Response 
+             * @brief HEAD request
+             *
+             * @param url
+             * @param kwargs
+             * @return Models::Response
              */
             Models::Response head(const std::string &url, py::kwargs kwargs = {});
 
             /**
-             * @brief OPTIONS request 
-             * 
-             * @param url 
-             * @param kwargs 
-             * @return Models::Response 
+             * @brief OPTIONS request
+             *
+             * @param url
+             * @param kwargs
+             * @return Models::Response
              */
             Models::Response options(const std::string &url, py::kwargs kwargs = {});
 
 
             /**
              * @brief Handles the __enter__ method of the Session class
-             * 
-             * @return Session* 
+             *
+             * @return Session*
              */
             Session* __enter__();
 
             /**
              * @brief Handles python side of the __exit__ method
-             * 
-             * @param exc_type 
-             * @param exc_value 
-             * @param traceback 
+             *
+             * @param exc_type
+             * @param exc_value
+             * @param traceback
              */
             void __exit__(const py::object &exc_type, const py::object &exc_value, const py::object &traceback);
         };
